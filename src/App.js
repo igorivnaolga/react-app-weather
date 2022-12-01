d29756f8991d8bec413208ae0e500547b5301c48
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-12-01 14:34:23 +1300

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was codded by Olga Sergiienko and is{" "}
+        <a
+          href="https://github.com/igorivnaolga/react-app-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
