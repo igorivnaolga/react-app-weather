253fcd25e55255f73e0387dd87ce1c4bdab75d35
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-12-09 15:33:08 +1300

    Fixed styling

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,7 +28,7 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="row">
+        <div className="row" mt-5>
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
